c24c06b32c70d66447a82b78f659bc40ed8a29ed
ravindradiaz <revindradiaz@gmail.com>
2026-06-07 15:48:10 +0700

    Membuat Dashboard Admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Import Komponen Halaman
 import Login from './pages/auth/Login';
+import AdminDashboard from './pages/admin/Dashboard';
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Rute Halaman Awal (Login) */}
+        <Route path="/" element={<Login />} />
+        
+        {/* Rute Halaman Admin */}
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+
+        {/* Nanti rute untuk Pimpinan dan Petugas tinggal ditambahkan di bawah sini */}
+        {/* <Route path="/petugas/dashboard" element={<PetugasDashboard />} /> */}
+        {/* <Route path="/pimpinan/dashboard" element={<PimpinanDashboard />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
