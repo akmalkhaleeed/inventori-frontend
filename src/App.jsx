649b4a11cbdada2af8eb1c4d9ea542f503410796
ravindradiaz <revindradiaz@gmail.com>
2026-06-07 15:08:59 +0700

    Membuat Halaman Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
+import Login from './pages/auth/Login';
+
 function App() {
-  return (
-    <div className="card">
-      <div className="badge">Official Greeting Card</div>
-      <h1>Hello World</h1>
-      <p>Selamat datang di tampilan resmi Hello World Sistem Inventori Barang. Desain mewah dan profesional ini dibuat menggunakan Vanilla CSS bawaan.</p>
-      <div className="status-box">
-        <span>✨</span> Status: Ready (React + Vite + Vanilla CSS)
-      </div>
-    </div>
-  );
+  return <Login />;
 }
 
 export default App;
